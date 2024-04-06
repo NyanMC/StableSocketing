97f1803cedd53938d3105aa39deb174d3cc62d49
ChromaNyan <nyancatz593@gmail.com>
2024-04-06 11:45:21 -0500

    initial commit

---
+++ src/main/java/com/chromanyan/stablesocketing/StableSocketing.java
@@ -0,0 +1,9 @@
+package com.chromanyan.stablesocketing;
+
+import net.minecraftforge.fml.common.Mod;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(StableSocketing.MODID)
+public class StableSocketing {
+    public static final String MODID = "stablesocketing";
+}
