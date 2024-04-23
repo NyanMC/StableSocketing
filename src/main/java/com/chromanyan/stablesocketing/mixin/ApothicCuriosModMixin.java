25bae1d57f7e42cd66e7434ca234f20480f2d48a
ChromaNyan <nyancatz593@gmail.com>
2024-04-23 16:44:53 -0500

    feat: fix the mod 2: electric boogaloo

--- src/main/java/com/chromanyan/stablesocketing/mixin/ApothicCuriosModMixin.java
+++ src/main/java/com/chromanyan/stablesocketing/mixin/ApothicCuriosModMixin.java
@@ -6,20 +6,20 @@ import net.minecraftforge.event.entity.player.ItemTooltipEvent;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
-import shadows.apotheosis.adventure.affix.socket.gem.Gem;
-import shadows.apotheosis.adventure.affix.socket.gem.GemInstance;
-import shadows.apotheosis.adventure.affix.socket.gem.bonus.GemBonus;
-import shadows.apotheosis.adventure.loot.LootCategory;
+import dev.shadowsoffire.apotheosis.adventure.affix.socket.gem.Gem;
+import dev.shadowsoffire.apotheosis.adventure.affix.socket.gem.GemInstance;
+import dev.shadowsoffire.apotheosis.adventure.affix.socket.gem.bonus.GemBonus;
+import dev.shadowsoffire.apotheosis.adventure.loot.LootCategory;
 
 import java.util.Optional;
 
 @Mixin(value = ApothicCuriosMod.class, remap = false)
 public class ApothicCuriosModMixin {
 
-    @Redirect(method = "removeTooltip(Lnet/minecraftforge/event/entity/player/ItemTooltipEvent;Lshadows/apotheosis/adventure/affix/socket/gem/GemInstance;Lnet/minecraft/world/item/ItemStack;)V", at = @At(value = "INVOKE", target = "Lshadows/apotheosis/adventure/affix/socket/gem/Gem;getBonus(Lshadows/apotheosis/adventure/loot/LootCategory;)Ljava/util/Optional;"))
+    @Redirect(method = "removeTooltip(Lnet/minecraftforge/event/entity/player/ItemTooltipEvent;Ldev/shadowsoffire/apotheosis/adventure/affix/socket/gem/GemInstance;Lnet/minecraft/world/item/ItemStack;)V", at = @At(value = "INVOKE", target = "Ldev/shadowsoffire/apotheosis/adventure/affix/socket/gem/Gem;getBonus(Ldev/shadowsoffire/apotheosis/adventure/loot/LootCategory;)Ljava/util/Optional;"))
     private Optional<GemBonus> actuallyGetBonus(Gem instance, LootCategory lootCategory, ItemTooltipEvent event, GemInstance gem, ItemStack stack) {
 
-        return instance.getBonus(lootCategory, gem.rarity());
+        return instance.getBonus(lootCategory, gem.rarity().get());
     }
 
 }
